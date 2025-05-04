5e5eb159e6edde0ed1208184d4bc20a9804e61a7
bikramsah <bikramsah646@gmail.com>
2025-05-04 15:59:06 +0545

    changed

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 echo '<h1>Welcome To Biratanagar Nepal</h1>';
-echo '<h2> We are Bikram and Suman From Nepal</h2>';
-echo'<h1> we are at Mahendra Morang Campus Biratnagar Nepal </h2>';
 echo'<img src="mnr.jpg"width="700">';
 
 ?>
