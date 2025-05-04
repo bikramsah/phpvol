e60f4042d93d70c691e561b304267bc5b5898116
bikramsah <bikramsah646@gmail.com>
2025-05-04 12:10:20 +0545

    my first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo '<h1>Welcome To Biratanagar Nepal!</h1>';
+echo '<h2> I am Bikram Sah From Sukhipur </h2>';
+echo '<img src="bikram.png"width="500">';
+echo'<img src="mnr.jpg"width="700">';
+
+?>
