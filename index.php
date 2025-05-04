bc08c23c580280a3f7e2d86fac81f28ff41e844b
bikramsah <bikramsah646@gmail.com>
2025-05-04 12:50:47 +0545

    some text changed

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 echo '<h1>Welcome To Biratanagar Nepal!</h1>';
-echo '<h2> I am Bikram Sah From Sukhipur </h2>';
-echo '<img src="bikram.png"width="500">';
+echo '<h2> I am Bikram Sah </h2>';
+
+
 echo'<img src="mnr.jpg"width="700">';
 
 ?>
